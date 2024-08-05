08d8b655066105b762c80dee8ad059584259322c
DeepakGandhi2007 <95476025+DeepakGandhi2007@users.noreply.github.com>
2024-08-05 17:14:12 +0530

    Update route.js

--- src/app/api/Lead/excel/route.js
+++ src/app/api/Lead/excel/route.js
@@ -4,62 +4,102 @@ import connect from "@/dbConfig/dbConfig";
 import Leads from "@/models/Leads";
 import TagsModel from "@/models/Tags";
 import StatusModel from "@/models/Status";
-
+import Source from "@/models/Source";
+import User from "@/models/Users";
+import jwt from 'jsonwebtoken';
 // Connect to the database
 connect();
 
-async function parseCSVAndSaveToDB(csvData, LeadStatus, Source) {
+async function parseCSVAndSaveToDB(csvData, LeadStatus, userIdyoken) {
     const parsedData = await parseCSV(csvData);
     const leadsToSave = [];
 
     for (const lead of parsedData) {
         console.log(lead);
-        const marketingtag = await TagsModel.findOne({ Tag: lead.marketingtags });
+        let marketingtag = await TagsModel.findOne({ Tag: lead.form_name });
         if (!marketingtag) {
-            console.log(`marketingtag with name ${lead.marketingtags} not found, skipping lead...`);
-            continue;
+            console.log(`Marketing tag with name ${lead.form_name} not found, creating new tag...`);
+            marketingtag = new TagsModel({ Tag: lead.form_name, AddBy: userIdyoken, });
+            try {
+                await marketingtag.save();
+                console.log(`Created new marketing tag with name ${lead.form_name}`);
+            } catch (error) {
+                console.error(`Error creating marketing tag ${lead.form_name}:`, error);
+                continue;
+            }
         }
 
         const marketingtagId = marketingtag._id;
-        console.log("marketingtagId", marketingtagId);
-        const existingLead = await Leads.findOne({ Phone: lead.Phone, marketingtags: marketingtagId });
+        console.log("Marketing tag ID:", marketingtagId);
 
+        const existingLead = await Leads.findOne({ Phone: lead.Phone, marketingtags: marketingtagId });
         if (existingLead) {
-            console.log(`Lead with phone number ${lead.Phone} and tag ${marketingtagtagId} already exists, skipping...`);
+            console.log(`Lead with phone number ${lead.Phone} and tag ${marketingtagId} already exists, skipping...`);
             continue;
         }
-        const dldtag = await TagsModel.findOne({ Tag: lead.tags });
-        const dldtagId = dldtag._id;
+        let dldtagId = "";
+        if (lead.tags) {
 
-        if (!existingLead) {
-            leadsToSave.push({
-                LeadStatus: LeadStatus || "6683f48a13502fdf0ea8e5a9",
-                Source: Source || "6683f48a13502fdf0ea8e5a9",
-                Assigned: lead.Assigned,
-                Name: lead.Name,
-                scoreupdateby: "",
-                Phone: lead.Phone,
-                AltPhone: lead.AltPhone,
-                Address: lead.Address,
-                Email: lead.Email,
-                typeprop: lead.typeprop,
-                City: lead.City,
-                State: lead.State,
-                Project: lead.Project,
-                Budget: lead.Budget,
-                Country: lead.Country,
-                Location: lead.Location,
-                ZipCode: lead.ZipCode,
-                Priority: lead.Priority,
-                Type: lead.Type,
-                Description: lead.Description,
-                LeadType: lead.LeadType,
-                Doneby: '65cb59f9c93306341828d9eb',
-                tags: dldtagId,
-                marketingtags: marketingtagId,
-                unitnumber: lead.unitnumber
-            });
+            const dldtag = await TagsModel.findOne({ Tag: lead.tags });
+            if (!dldtag) {
+                console.log(`DLD tag with name ${lead.tags} not found, skipping lead...`);
+                continue;
+            }
+            dldtagId = dldtag._id;
         }
+
+        const user = await User.findOne({
+            username: { $regex: new RegExp(lead.agent, 'i') }
+        });
+        if (!user) {
+            console.log(`User with username ${lead.agent} not found, skipping lead...`);
+            continue;
+        }
+        const userid = user._id;
+
+        let source = "";
+        if (lead.source === "fb") {
+            source = "SM3";
+        } else if (lead.source === "ig") {
+            source = "SM4";
+        }
+        const sourcelead = await Source.findOne({ Source: source });
+        if (!sourcelead) {
+            console.log(`Source with name ${source} not found, skipping lead...`);
+            continue;
+        }
+        const sourceid = sourcelead._id;
+
+        leadsToSave.push({
+            LeadStatus: LeadStatus || "6683f48a13502fdf0ea8e5a9",
+            Source: sourceid || "6683f48a13502fdf0ea8e5a9",
+            Assigned: userid,
+            Name: lead.full_name,
+            scoreupdateby: "",
+            Phone: lead.phone_number,
+            AltPhone: lead.whatsapp_number,
+            Address: "",
+            Email: lead.email,
+            typeprop: "",
+            City: "",
+            State: "",
+            Project: "",
+            Budget: lead.what_is_your_budget_,
+            Country: "",
+            Location: "",
+            ZipCode: "",
+            Priority: "",
+            Type: "",
+            Description: "",
+            LeadType: "marketing",
+            Doneby: '65cb59f9c93306341828d9eb',
+            tags: dldtagId || null,
+            marketingtags: marketingtagId,
+            unitnumber: "",
+            campagincountry: lead.country,
+            campaignid: lead.campaign_id,
+            adid: lead.ad_id
+        });
     }
 
     if (leadsToSave.length > 0) {
@@ -67,11 +107,14 @@ async function parseCSVAndSaveToDB(csvData, LeadStatus, Source) {
         const batchSize = 1000;
         for (let i = 0; i < leadsToSave.length; i += batchSize) {
             const batch = leadsToSave.slice(i, i + batchSize);
-            await Leads.insertMany(batch);
+            try {
+                await Leads.insertMany(batch);
+            } catch (error) {
+                console.error(`Error inserting batch ${i / batchSize + 1}:`, error);
+            }
         }
     }
 }
-
 
 function parseCSV(csvData) {
     return new Promise((resolve, reject) => {
@@ -92,13 +135,15 @@ export async function POST(request) {
     try {
         const reqBody = await request.json();
         const { file, LeadStatus, Source } = reqBody;
-
+        const token = request.cookies.get('token')?.value || '';
+        const decoded = jwt.decode(token);
+        const userIdyoken = decoded.id;
         if (!file) {
             throw new Error('No file provided');
         }
 
         const csvData = atob(file);
-        await parseCSVAndSaveToDB(csvData, LeadStatus, Source);
+        await parseCSVAndSaveToDB(csvData, LeadStatus, userIdyoken);
 
         return NextResponse.json({ message: "CSV parsed and saved to database successfully" }, { status: 200 });
 
